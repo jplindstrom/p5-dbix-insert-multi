2c7d7c925fec79ed42ada8cbed80e31f75a64ce1
Johan Lindstrom <johan.lindstrom@broadbean.com>
2019-09-08 17:20:45 +0000

    master: Modules

--- lib/DBIx/Insert/Multi.pm
+++ lib/DBIx/Insert/Multi.pm
@@ -1,28 +1,156 @@
 package DBIx::Insert::Multi;
-use 5.008001;
-use strict;
-use warnings;
+use 5.010000;
 
-our $VERSION = "0.01";
-
-
-
-1;
-__END__
-
-=encoding utf-8
+use Moo;
 
 =head1 NAME
 
-DBIx::Insert::Multi - It's new $module
+DBIx::Insert::Multi -- Insert multiple table rows in a single statement
 
 =head1 SYNOPSIS
 
     use DBIx::Insert::Multi;
 
+    my $dbh = DBI->connect(...);
+
+    my $multi = DBIx::Insert::Multi->new({ dbh => $dbh, });
+    $multi->insert(
+        book => [
+            {
+                title            => "Winnie the Pooh",
+                author           => "Milne",
+                publication_date => DateTime->new(year => 1926)->ymd,
+            },
+            {
+                title            => "Paddington",
+                author           => "Bond",
+                publication_date => DateTime->new(year => 1958)->ymd,
+            },
+        ],
+    );   # die on error
+
+    # Database specific INSERT statement
+    # MySQL: don't stop on errors
+    my $multi = DBIx::Insert::Multi->new({
+        ...
+        insert_sql_fragment => "INSERT IGNORE INTO",
+    });
+
 =head1 DESCRIPTION
 
-DBIx::Insert::Multi is ...
+Bulk insert many db rows using a single INSERT INTO statement, e.g.
+
+    INSERT INTO book (author, publication_date, title) VALUES
+        ( ?, ?, ? ),
+        ( ?, ?, ? );
+
+=head2 Restrictions
+
+All the hashrefs with row data should be shaped the same, i.e. have
+the same keys.
+
+You should only use values that can be inserted into a database.
+
+That means no data structures (refs), and no objects. However, objects
+will be stringified, so if they have overloaded stringification that
+will work.
+
+Note that L<DateTime> objects are stringified to a format that is
+unlikely to work correctly with your database date format (and without
+a timezone), so make sure you construct strings manually before
+inserting them.
+
+Undefs become NULL as usual.
+
+
+
+=head1 ISSUES
+
+=head2 last_insert_id
+
+It may be that you need to get hold of the PK ids of the inserted
+rows. This is very non-standard and fiddly, so at this point this
+module doesn't officially do any of that.
+
+You can do this yourself though, but I wouldn't bet it's very
+reliable.
+
+    $dbh->last_insert_id(undef, undef, $table, undef);
+
+Calling "$dbh->last_insert_id" returns a newly inserted row PK
+value. It seems to vary between databases whether this is the id of
+the first row or the last one. For instance:
+
+=over
+
+=item MySQL: first
+
+=item Postgres: last
+
+=back
+
+If the PK is an auto-increment / sequence, it is probably not
+B<guaranteed> that these ids are in an unbroken series, but at least
+MySQL B<seems> to do that.
+
+Read more about all the caveats here: L<DBI/"last_insert_id">.
+
+
+=head3 Returning ids
+
+Some databases (Postgres) support INSERT INTO ... RETURNING, which can
+be used to retrieve data from the inserted rows. This seems to be the
+only reliable way to do this.
+
+This module can't do this at the moment, but patches are
+welcome. Please submit a bug to open a discission about what the API
+should look like.
+
+=cut
+
+use DBIx::Insert::Multi::Batch;
+
+
+
+has dbh => ( is => "ro", required => 1 );
+
+has insert_sql_fragment => ( is => "lazy" );
+sub _build_insert_sql_fragment { "INSERT INTO" }
+
+has is_last_insert_id_required => ( is => "lazy" );
+sub _build_is_last_insert_id_required { 0 }
+
+
+
+=head1 METHODS
+
+=head2 insert($table_name, $records_arrayref)
+
+Perform the insert into $table_name of all the rows in
+$records_arrayref (arrayref with hashrefs, where the hashref keys are
+the column names, and the values are the column values).
+
+The return value not specified. If the query fails, die.
+
+=cut
+
+sub insert {
+    my $self = shift;
+    my ($table, $records) = @_;
+    return DBIx::Insert::Multi::Batch->new({
+        dbh                        => $self->dbh,
+        insert_sql_fragment        => $self->insert_sql_fragment,
+        is_last_insert_id_required => $self->is_last_insert_id_required,
+        table                      => $table,
+        records                    => $records,
+    })->insert();
+}
+
+
+
+1;
+
+
 
 =head1 LICENSE
 
@@ -33,7 +161,6 @@ it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-Johan Lindstrom E<lt>johan.lindstrom@broadbean.comE<gt>
+Johan Lindstrom E<lt>johanl@cpan.orgE<gt>
 
 =cut
-
