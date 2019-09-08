fd2bec4aece1de42a6a626748018a3a0a1951447
Johan Lindstrom <johan.lindstrom@broadbean.com>
2019-09-08 17:05:18 +0000

    master: Initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+DBIx::Insert::Multi - It's new $module
+
+# SYNOPSIS
+
+    use DBIx::Insert::Multi;
+
+# DESCRIPTION
+
+DBIx::Insert::Multi is ...
+
+# LICENSE
+
+Copyright (C) Johan Lindstrom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Johan Lindstrom <johanl@cpan.org>
---
+++ lib/DBIx/Insert/Multi.pm
@@ -0,0 +1,39 @@
+package DBIx::Insert::Multi;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Insert::Multi - It's new $module
+
+=head1 SYNOPSIS
+
+    use DBIx::Insert::Multi;
+
+=head1 DESCRIPTION
+
+DBIx::Insert::Multi is ...
+
+=head1 LICENSE
+
+Copyright (C) Johan Lindstrom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Johan Lindstrom E<lt>johan.lindstrom@broadbean.comE<gt>
+
+=cut
+
